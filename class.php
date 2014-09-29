1c67d15fe381f9597ad096df5f798b9c53584512
Juan Mora <jvmora@fen.uchile.cl>
2014-09-29 14:40:01 -0400

    Se agregan nuevas funciones de validacion y saniamiento

--- class.php
+++ class.php
@@ -2,20 +2,20 @@
 
 class Sanear{
 
-	#solo funciona en chrome
+	# Funcion no compatible con Firefox
 	public function xssproteccion($nivel){
 		#nivel = 1 | Activa Proteccion
 		#nivel = 0 | Desactiva Proteccion
 		header("X-XSS-Protection:".$nivel." ");
 	}
 
-	protected function saneartexto($string){
-		$sano = strip_tags($string);
+	protected function saneartextohtml($string, $cadenahtml=''){
+		$sano = strip_tags($string,$cadenahtml);
 		return $sano;
 	}
 
-	protected function sanearmail($email){
-		$sano = filter_var($email,FILTER_SANITIZE_EMAIL);
+	protected function sanearmail($mail){
+		$sano = filter_var($mail,FILTER_SANITIZE_EMAIL);
 		return $sano;
 	}
 
@@ -23,7 +23,25 @@ class Sanear{
 		$sano = filter_var($numero,FILTER_SANITIZE_NUMBER_INT);
 		return $sano;
 	}
+
+	protected function sanearurl($url){
+		$sano = filter_var($url, FILTER_SANITIZE_URL);
+		return $sano;
+	}
 }
+
+
+class Validar{
+	
+	public function validarmail($mail){
+		$validado = filter_var($mail, FILTER_VALIDATE_EMAIL);
+		return $validado;
+	}
+}
+
+###################################################################################
+############################ Ejemplos de Uso en Clases ############################
+###################################################################################
 
 class Datos extends Sanear{
 	public function correo($correo){
@@ -31,6 +49,12 @@ class Datos extends Sanear{
 	}
 	public function numeros($numero){
 		return Sanear::sanearnumero($numero);
+	}
+	public function texto($texto, $html=""){
+		return Sanear::saneartextohtml($texto, $html);
+	}
+	public function url($url){
+		return Sanear::sanearurl($url);
 	}
 }
 
--- index.php
+++ index.php
@@ -5,13 +5,22 @@ include ("class.php");
 $sanear = new Sanear();
 $datos 	= new Datos();
 
-$sanear->xssproteccion(0);
+$sanear->xssproteccion(1);
 
 
 echo $_GET['url'];
 echo "<br><br>";
-echo $datos->correo('raicerk>@gmail.com');
+
+echo $datos->correo('raicerk<>@gmail.com');
 echo "<br><br>";
+
 echo $datos->numeros(1) + $datos->numeros("2 dsadsa");
+echo "<br><br>";
+
+echo $datos->texto("<b>hola </b> <li>jdskdjs</li>");
+echo "<br><br>";
+
+echo $datos->url("http://raicerk.cl/goog ssd  l");
+echo "<br><br>";
 
 ?>
