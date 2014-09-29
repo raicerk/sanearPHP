83f6485105c209ed77fe4274eaf7341e0070a483
Juan Mora - Linux <jvmora@raicerk.cl>
2014-09-28 23:14:49 -0300

    Prueba de saneamiento con clases creadas

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
+include ("class.php");
 
+$sanear = new Sanear();
+$datos 	= new Datos();
+
+$sanear->xssproteccion(0);
+
+
+echo $_GET['url'];
+echo "<br><br>";
+echo $datos->correo('raicerk>@gmail.com');
+echo "<br><br>";
+echo $datos->numeros(1) + $datos->numeros("2 dsadsa");
 
 ?>
